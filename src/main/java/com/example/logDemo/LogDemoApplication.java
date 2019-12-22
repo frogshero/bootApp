952c0ac5083f5a6d1bf243566a1dfe2d0019aabf
frogshero <frogshero@sina.cn>
2019-12-22 16:17:13 +0800

    test

--- src/main/java/com/example/logDemo/LogDemoApplication.java
+++ src/main/java/com/example/logDemo/LogDemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.logDemo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,9 +17,12 @@ public class LogDemoApplication {
 		SpringApplication.run(LogDemoApplication.class, args);
 	}
 	
+	@Value("${testVal}")
+	private String testVal;
+	
 	@GetMapping("/test")
 	public void test() {
-		log.info("aaaa");
+		log.info(testVal);
 	}
 
 }
